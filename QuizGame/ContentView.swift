f936dd66b757bb045a1778a264144d9924b4d71c
Chetan <chetan.mn9@gmail.com>
2025-05-19 22:22:48 +1000

    Initial Commit

---
+++ QuizGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizGame
+//
+//  Created by Chetan on 19/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Chetan on 19/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
